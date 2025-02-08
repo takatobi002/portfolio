f4514343aaad49b15bb903cf974b4295d481fb73
Taichi <takatobi002@gmail.com>
2025-02-08 13:29:51 +0900

    不要なFastAPI関連のコードを削除

--- app/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
-from pathlib import Path
-
-app = FastAPI(title="研究者ポートフォリオ")
-
-# 静的ファイルの提供
-app.mount("/static", StaticFiles(directory="static"), name="static")
-app.mount("/", StaticFiles(directory="site", html=True), name="site") 
